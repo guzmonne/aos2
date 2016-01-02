92d6b9614b7fb786fc6fe277abc70fa9535cb68b
Guzman Monne <guzmonne@hotmail.com>
2016-01-02 17:53:07 -0300

    added react-router

--- src/app.js
+++ src/app.js
@@ -3,7 +3,18 @@ require('../styles/override.styl');
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { syncReduxAndRouter } from 'redux-simple-router'
 import { store } from './state/store.js'
 import MainApp from './main-app.js'
 
-render( <Provider store={store}><MainApp/></Provider>, document.getElementById('root') );
+syncReduxAndRouter(browserHistory, store)
+
+render(
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/" component={MainApp} />
+		</Router>
+	</Provider>
+	, document.getElementById('root')
+);
--- src/state/main-reducer.js
+++ src/state/main-reducer.js
@@ -1,7 +1,9 @@
+import { routeReducer } from 'redux-simple-router'
 import counterReducer from '../counter.reducer.js'
 
 export default function mainReducer( state={}, action){
 	return {
-		counter: counterReducer(state.counter, action)
+		counter: counterReducer(state.counter, action),
+		routing: routeReducer(state.routing, action)
 	}
 }
