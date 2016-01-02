9e0acb0aef43967cb0cf5e7159b9808acacb76e4
Guzman Monne <guzmonne@hotmail.com>
2016-01-02 19:39:27 -0300

    added test runner karma to the project

--- src/state/store.spec.js
+++ src/state/store.spec.js
@@ -5,8 +5,17 @@ describe('store', () => {
 
 	it('should initialize', () => {
 		const actual = store.getState();
+
+		console.log(store.getState())
+
 		const expected = {
-			couter: 0
+			counter: 0,
+			routing: { 
+				changeId: 1, 
+				path: undefined, 
+				replace: false, 
+				state: undefined
+			}
 		}
 
 		expect(actual).toEqual(expected);
